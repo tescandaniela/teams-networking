d5ea0fbd91bb4c28d0b3085cef9540724f015f03
Tescan Daniela <tescandaniela6@gmail.com>
2024-02-19 21:36:01 +0200

    in js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import "./style.css";
 
 console.info("app ready");
+
+function loadTeams() {
+  fetch("http://localhost:3000/teams-json")
+    .then(r => r.json())
+    .then(teams => {
+      console.info(teams);
+      return teams;
+    });
+  console.warn("loadTeams", promise);
+}
+
+loadTeams();
